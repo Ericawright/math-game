32e83ba9216a10fb464f5923e4ffa8e03b2c17bd
Erica Wright <ericaewright@gmail.com>
2015-04-03 18:19:09 +0000

    initial commit

---
+++ mathgame.rb
@@ -0,0 +1,35 @@
+
+def init
+puts 'Hello, welcome to the math game, player one, please enter your name'
+player1 = gets.chomp
+puts 'Player two, please enter your name'
+player2 = gets.chomp 
+end  
+
+def user_names(player1, player2)
+  @player1 = player1
+  @player2 = player2
+end
+
+def generate_question
+
+end 
+
+def randomize
+
+end   
+
+def prompt_player
+
+end
+
+
+def verify_answer
+
+end
+
+def restart_game
+
+end
+
+init
