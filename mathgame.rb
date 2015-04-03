938e06f13151dd1952bc58d21e7ba26416ea9901
Erica Wright <ericaewright@gmail.com>
2015-04-03 20:58:23 +0000

    add points

--- mathgame.rb
+++ mathgame.rb
@@ -46,36 +46,54 @@
 
 @player1 = {
   name: 'player 1',
-  lives: 3
+  lives: 3,
+  points: 0
 }
 @player2 = {
   name: 'player 2',
-  lives: 3
+  lives: 3,
+  points: 0
 }
+@current_player = @player1
+@other_player = @player2
 
 while @player1[:lives] > 0
+
   def generate_question
     a = rand(1..20)
     b = rand(1..20)
     @answer = a + b
     puts "What does #{a} plus #{b} equal?"
     user_input
-
   end
 
   def user_input
     user_answer = gets.chomp.to_i
     if user_answer == @answer
       puts 'good job'
+      @current_player[:points] += 1
+     puts  @current_player[:points]
     else 
       puts 'wrong'
       @player1[:lives] -= 1
+    end
+    player_switch
+  end
+
+   def player_switch
+    if @current_player  = @player1
+      @current_player = @player2
+      @other_player = @player1
+    else
+      @current_player = @player1
+      @other_player = @player2
     end
   end
 
   generate_question
 
 end
+puts:"#{@current_player[:name]}, you lose! #{@other_player[:name]}'s score was #{@other_player[:points]}"
 
 
 
